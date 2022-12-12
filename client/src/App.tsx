c9cd45c8d500a51f59f687b6068005c854b13dff
kwanwchan9 <kwanwchan9@gmail.com>
2022-12-12 22:46:25 +0800

    Chnage BrowserRouter to HashRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import CreatePost from './pages/CreateReview'
@@ -8,14 +8,16 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/viewpost' element={<ViewPost />}></Route>
         <Route path='/createpost' element={<CreatePost />}></Route>
       </Routes>
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   )
 }
 
